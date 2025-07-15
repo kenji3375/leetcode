313cc51daf2d7917e0ada87f110b714c6c05253d
kenji3375 <lukborsuk@gmail.com>
2025-07-15 02:36:57 +0200

    works a little bit:3

--- regular_expression_matching/main.cpp
+++ regular_expression_matching/main.cpp
@@ -21,20 +21,63 @@ b
 
     */
     
-    for (int y=0; y<plen; ++y) {
-        for (int x=0; x<slen; ++x) {
+    // for (int y=0; y<plen; ++y) {
+    //     for (int x=0; x<slen; ++x) {
             
+    //     }
+    // }
+
+    //assuming the p is going to be correct
+
+    bool correct = true;
+    int i = 0;
+    for (int j=0; j<plen; ++j) {
+        if(!correct || i==slen)    break;
+        correct ^= (bool)(char)1;
+
+        if (j<plen-1) {
+            if(p[j+1] == '*') {
+
+                //------
+                // here we check if any amount of character before the * fits;
+                //------
+                
+                
+                if(s[i] != p[j]) {
+                    ++i;
+                } else {
+                    while(s[i] == p[j]) {
+                        ++i;
+                    }
+                }
+                correct = true;
+                //------
+                ++j;
+            } else {
+                if (p[j] == s[i]) {
+                    correct = true;
+                    ++i;
+                }
+            }
+        } else {        //  the last j
+            if(j>0 && p[j] == '*') {
+                while(i<slen && s[i] == p[j-1]) {
+                    ++i;
+                }
+                if(i == slen)   correct = true;
+            }
+            else  if(s[i] == p[j])    correct = true;
         }
     }
 
     
-    return false;
+    return correct;
 }
 
 
 int main (void) {
     string test1="aa",test2="a*";
-    test1="aa",test2="a.";
+    test1="abbba",test2="aba";
     
 
     cout<<test1<<" <---> "<<test2<<endl<<(isMatch(test1,test2) ? "match":"don't match")<<endl;
